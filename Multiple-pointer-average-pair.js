cc4e0aad07b38a626a8ecbbed9897c7f8410c9e8
Abhishek Gupta <gupta.abhishek2996@gmail.com>
2020-09-23 01:55:08 +0530

    averagePair-multiple-pointer

---
+++ Multiple-pointer-average-pair.js
@@ -0,0 +1,11 @@
+// Given a sorted array of integers and a target average, determine if there is a
+// pair of values in array whose average equals target average
+
+function averagePair(nums, avg) {
+    let head = 0;
+    let tail = nums.length-1;
+    while(head<tail) {
+        let average = (nums[head]+nums[tail])/2;
+        
+    }
+}
